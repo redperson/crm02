5154ab08c65deff814cbecec461c5ec8ae35a43b
redperson <18334727491@163.com>
2016-05-27 22:01:18 +0800

    Update Test.java

--- src/com/atguigu/test/git/Test.java
+++ src/com/atguigu/test/git/Test.java
@@ -4,5 +4,8 @@ public class Test {
   public static void main(String[] args) {
 	System.out.println("hello---");
 	system.out.println("hello====");
+	System.out.println("hello---");
+	System.out.println("hello---");	
+	System.out.println("hello---");
 }
 }
